760776ca74c86c894ed282d99b7d2745f06cc9a1
Nicolas-hr00 <80604439+Nicolas-hr00@users.noreply.github.com>
2024-12-08 21:36:18 -0600

    front end menu

--- app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.plcoding.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,25 +11,81 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import com.plcoding.stockmarketapp.databinding.ActivityMainBinding
 //import com.plcoding.stockmarketapp.presentation.NavGraphs
 import com.plcoding.stockmarketapp.presentation.company_listings.NavGraphs
- import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
+import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint // lets us inject stuff into our android components
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            StockMarketAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    DestinationsNavHost(navGraph = NavGraphs.root) //this is a generated file
-                }
+        super.onCreate(savedInstanceState);
+//      setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemReselectedListener { item ->
+
+            when(item.itemId) {
+                R.id.home -> replaceFragment(Home());
+                R.id.profile -> replaceFragment(Profile());
+
+
+                else -> false
+
             }
+            true;
+
         }
     }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 }
+
+//package com.plcoding.stockmarketapp
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Surface
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+////import com.plcoding.stockmarketapp.presentation.NavGraphs
+//import com.plcoding.stockmarketapp.presentation.company_listings.NavGraphs
+// import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
+//import com.ramcosta.composedestinations.DestinationsNavHost
+//import dagger.hilt.android.AndroidEntryPoint
+//
+//@AndroidEntryPoint // lets us inject stuff into our android components
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            StockMarketAppTheme {
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    DestinationsNavHost(navGraph = NavGraphs.root) //this is a generated file
+//                }
+//            }
+//        }
+//    }
+//}
